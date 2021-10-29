a5d5068dad94ec0c8dad10fcf861a4f3d9606bb2
NightRyderrr <48996537+NightRyderrr@users.noreply.github.com>
2021-10-28 22:12:30 -0500

    Add files via upload

---
+++ Color.py
@@ -0,0 +1,45 @@
+
+
+
+#reds
+dark_red = 102 , 0 , 0
+red = 255, 0 ,0
+light_red = 255, 153, 153
+
+#orange
+dark_orange = 102, 51, 0
+orange = 255 ,128 , 0
+light_orange = 255, 178 , 102
+
+#yellow
+dark_yellow = 102 , 102 , 0
+yellow =  255, 255, 0
+light_yellow = 255, 255 , 153
+
+#green
+
+dark_green = 51 , 102 , 0
+green = 129 , 255 , 0
+light_green = 204 , 255 ,153
+normal_green = 0 ,255, 0
+
+#blue
+dark_blue = 0 , 102, 102
+cyan = 0, 255, 255
+light_blue = 153, 255, 255
+normal_blue = 0 , 0 ,255
+
+#purples
+dark_purple = 51 , 0 , 102
+purple = 127 , 0 , 255
+light_purple = 204 , 153 , 255
+
+#pinks
+pink = 255 , 0 , 255
+
+#misc
+white = 255, 255, 255
+black = 0 , 0 , 0
+gray = 96 , 96 , 96
+rich_black = 1, 11 ,19
+
